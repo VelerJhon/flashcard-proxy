b5f29f788915ab49f86028cc29ee78f3bc973cc5
VelerJhon <yokoiori0914@gmail.com>
2026-02-27 11:29:58 +0900

    sw.js を更新
    背景色変え

--- sw.js
+++ sw.js
@@ -1,28 +1,28 @@
-const CACHE_NAME = 'flashcards-v1';
+const CACHE_NAME = ‘flashcards-v2’;
 const ASSETS = [
-  '/',
-  '/index.html',
-  'https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&family=Noto+Sans+JP:wght@400;500;700&display=swap'
+‘/’,
+‘/index.html’,
+‘https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&family=Noto+Sans+JP:wght@400;500;700&display=swap’
 ];
 
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).catch(() => {})
-  );
-  self.skipWaiting();
+self.addEventListener(‘install’, e => {
+e.waitUntil(
+caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).catch(() => {})
+);
+self.skipWaiting();
 });
 
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
+self.addEventListener(‘activate’, e => {
+e.waitUntil(
+caches.keys().then(keys =>
+Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+)
+);
+self.clients.claim();
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request))
-  );
+self.addEventListener(‘fetch’, e => {
+e.respondWith(
+caches.match(e.request).then(cached => cached || fetch(e.request))
+);
 });
